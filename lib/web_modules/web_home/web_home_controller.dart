f6e617176637bad302ad031b6036ee35df7c347f
NavinKumarChahar <navinflutterdeveloper@gmail.com>
2026-05-25 14:08:20 +0530

    ready live

--- lib/web_modules/web_home/web_home_controller.dart
+++ lib/web_modules/web_home/web_home_controller.dart
@@ -1,6 +1,8 @@
 // web_modules/web_home/web_home_controller.dart
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:web/web.dart' as webHndle;
+import 'dart:js_interop';
 
 class WebHomeController extends GetxController {
   final ScrollController scrollController = ScrollController();
@@ -9,6 +11,39 @@ class WebHomeController extends GetxController {
 
   void setActive(int i) => activeItemIndex.value = i;
   void setQuery(String q) => searchQuery.value = q.toLowerCase();
+
+  @override
+  void onInit() {
+    super.onInit();
+    _listenForUpdates();
+  }
+
+  void _listenForUpdates() {
+    // Listen for the custom event dispatched from index.html
+    webHndle.window.addEventListener(
+      'flutter_version_update',
+      ((webHndle.Event event) {
+            _showUpdateNotification();
+          }).toJS
+          as webHndle.EventListener, // Explicitly cast the JS-interop object
+    );
+  }
+
+  void _showUpdateNotification() {
+    ScaffoldMessenger.of(Get.context!).showSnackBar(
+      SnackBar(
+        content: const Text('A new update is available!'),
+        duration: const Duration(days: 1), // Keep open until actioned
+        action: SnackBarAction(
+          label: 'Refresh',
+          onPressed: () {
+            // Force reload the web page to load the new deployment
+            webHndle.window.location.reload();
+          },
+        ),
+      ),
+    );
+  }
 
   @override
   void onClose() {
